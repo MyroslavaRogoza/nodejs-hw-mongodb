a295a20875e4437c1e3ef548b818271f50dd1838
MyroslavaRogoza <miroslavarogozayyy@gmail.com>
2024-06-12 22:57:26 +0300

    pagination

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,8 +1,26 @@
+import { calculatePaginationData } from '../utils/calculatePaginationData.js';
 import { ContactsCollection } from '../db/models/contact.js';
 
-export const getAllContacts = async () => {
-  const contacts = await ContactsCollection.find();
-  return contacts;
+export const getAllContacts = async ({ page = 1, perPage = 5 }) => {
+  const limit = perPage;
+  const skip = (page - 1) * perPage;
+
+  const contactsQuery = ContactsCollection.find();
+
+  // const contactsCount = await ContactsCollection.find().countDocuments();
+
+  // const contacts = await contactsQuery.skip(skip).limit(limit).exec();
+
+
+  const [contactsCount, contacts] = await Promise.all([
+    ContactsCollection.find().countDocuments(),
+    contactsQuery.skip(skip).limit(limit).exec(),
+  ]);
+  const paginationData = calculatePaginationData(contactsCount, perPage, page);
+  return {
+    data: contacts,
+    ...paginationData,
+  };
 };
 
 export const getContactById = async (contactId) => {
