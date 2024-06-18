ee8bbe7718921055f1778209911125ca9352db8a
Myroslava Rogoza <myroslavarogoza@gmail.com>
2024-06-18 18:42:03 +0300

    User and Session models

---
+++ src/db/models/user.js
@@ -0,0 +1,22 @@
+import { model, Schema } from 'mongoose';
+
+const User = new Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+    },
+    { timestamps: true, versionKey: false },
+);
+
+export default model('users', User);
