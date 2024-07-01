8cbffa3f994faece3d1f70a86f2ca0fbf0e71d51
Myroslava Rogoza <myroslavarogoza@gmail.com>
2024-07-01 16:59:18 +0300

    update

--- src/utils/saveToCloudinary.js
+++ src/utils/saveToCloudinary.js
@@ -9,6 +9,7 @@ cloudinary.v2.config({
   api_secret: env(CLOUDINARY.API_SECRET),
 });
 
+
 export const saveFileToCloudinary = async (file) => {
   const response = await cloudinary.v2.uploader.upload(file.path);
   return response.secure_url;
